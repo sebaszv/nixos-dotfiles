166bc637374ecd14cd5dc25160e4e7d215f310ac
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-05-23 18:03:30 -0400

    flake: Set up flake
    - Add and lock inputs.
    - Add formatter.
    - Add formatting check.
    - Add development shell.
    - Set up direnv.
    - Ignore Nix and direnv artifacts.

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs = {
+        systems.follows = "systems";
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+        darwin.follows = "";
+      };
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          treefmt =
+            (inputs.treefmt-nix.lib.evalModule pkgs {
+              projectRootFile = "flake.lock";
+              programs.nixfmt.enable = true;
+            }).config.build.wrapper;
+
+          pre-commit = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks.treefmt = {
+              enable = true;
+              package = treefmt;
+              pass_filenames = false;
+            };
+          };
+        in
+        {
+          checks.pre-commit = pre-commit;
+          formatter = treefmt;
+          devShells.default = pkgs.mkShellNoCC {
+            inherit (pre-commit) shellHook;
+            packages = [
+              pkgs.git
+              pkgs.nil
+            ];
+          };
+        };
+    };
+}
